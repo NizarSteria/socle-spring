9519929b7b3929c857b99569f0a0259b5fb402a1
Nizar Aouissaoui <aouissaoui.nizar@gmail.com>
2017-06-10 21:23:03 +0200

    initiate project from spring initializr

---
+++ src/main/java/tn/demo/SocleSpringApplication.java
@@ -0,0 +1,12 @@
+package tn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocleSpringApplication.class, args);
+	}
+}
